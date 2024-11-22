9b81bec5063731caf79c3461f3270cb62b5e0fa1
Marawan33 <marawan.mohamed112233@gmail.com>
2024-11-22 20:45:39 +0200

    Part 07 Basket Controller With Caching

---
+++ Store.Web/Controllers/BasketController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Service.Services.BasketServices;
+using Store.Service.Services.BasketServices.Dtos;
+
+namespace Store.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerBasketDto>> GetBasketAsync(string id)
+            => Ok(await _basketService.GetBasketAsync(id));
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasketDto>> UpdateBasketAsync(CustomerBasketDto customerBasketDto)
+            => Ok(await _basketService.UpdateBasketAsync(customerBasketDto));
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CustomerBasketDto>> DeleteBasketAsync(string id)
+            => Ok(await _basketService.DeleteBasketAsync(id));
+    }
+}
